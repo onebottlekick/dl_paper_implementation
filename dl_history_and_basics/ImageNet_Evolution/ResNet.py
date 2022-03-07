24ab10079f70b6510594240a0b4b5d33b235bfcd
onebottlekick <gksqudcks97@gmail.com>
2022-03-07 21:10:14 +0900

    .

--- dl_history_and_basics/ImageNet_Evolution/ResNet.py
+++ dl_history_and_basics/ImageNet_Evolution/ResNet.py
@@ -3,7 +3,7 @@ from torch import nn
 
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, intermediate_channels, identity_downsample=None):
+    def __init__(self, in_channels, intermediate_channels, identity_downsample=None, stride=1):
         super(ConvBlock, self).__init__()
         self.identity_downsample = identity_downsample
         self.expansion = 4
@@ -11,7 +11,7 @@ class ConvBlock(nn.Module):
         self.conv1 = nn.Conv2d(in_channels, intermediate_channels, kernel_size=1, stride=1, padding=0, bias=False)
         self.batchnorm1 = nn.BatchNorm2d(intermediate_channels)
         
-        self.conv2 = nn.Conv2d(intermediate_channels, intermediate_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(intermediate_channels, intermediate_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.batchnorm2 = nn.BatchNorm2d(intermediate_channels)
         
         self.conv3 = nn.Conv2d(intermediate_channels, intermediate_channels*self.expansion, kernel_size=1, stride=1, padding=0, bias=False)
@@ -33,12 +33,68 @@ class ConvBlock(nn.Module):
     
 
 class ResNet(nn.Module):
-    def __init__(self):
-        pass
-    
-    def forward(self):
-        pass
-    
-    def _layer(self):
-        pass
+    def __init__(self, block, layers, image_channels, num_classes):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.batchnorm1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         
+        self.layer1 = self._layer(block, layers[0], intermediate_channels=64, stride=1)
+        self.layer2 = self._layer(block, layers[1], intermediate_channels=128, stride=2)
+        self.layer3 = self._layer(block, layers[2], intermediate_channels=256, stride=2)
+        self.layer4 = self._layer(block, layers[3], intermediate_channels=512, stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512*4, num_classes)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.batchnorm1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc(x)
+        return x
+    
+    def _layer(self, block, num_residual_blocks, intermediate_channels, stride):
+        identity_downsample = None
+        layers = []
+        
+        if stride != 1 or self.in_channels != intermediate_channels*4:
+            identity_downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, intermediate_channels*4, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(intermediate_channels*4)
+            )
+            
+        layers.append(block(self.in_channels, intermediate_channels, identity_downsample, stride))
+        self.in_channels = intermediate_channels*4
+        
+        for i in range(num_residual_blocks - 1):
+            layers.append(block(self.in_channels, intermediate_channels))
+        
+        return nn.Sequential(*layers)
+
+
+def ResNet50(img_channel=3, num_classes=1000):
+    return ResNet(ConvBlock, [3, 4, 6, 3], img_channel, num_classes)
+
+
+def ResNet101(img_channel=3, num_classes=1000):
+    return ResNet(ConvBlock, [3, 4, 23, 3], img_channel, num_classes)
+
+
+def ResNet152(img_channel=3, num_classes=1000):
+    return ResNet(ConvBlock, [3, 8, 36, 3], img_channel, num_classes)
+
+
+if __name__ == '__main__':
+    net = ResNet101()
+    x = torch.randn(1, 3, 224, 224)
+    print(net(x).shape)
