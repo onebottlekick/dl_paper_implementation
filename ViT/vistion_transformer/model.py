f1f2ce50ff44011a6bf270000a1aada90ccf057c
onebottlekick <gksqudcks97@gmail.com>
2022-07-20 16:02:38 +0900

    shape annotation

--- ViT/vistion_transformer/model.py
+++ ViT/vistion_transformer/model.py
@@ -11,36 +11,45 @@ class ViT(nn.Module):
         super().__init__()
         
         img_shape = (img_channels,) + pair(img_size)
-        num_patches = int(np.sqrt(np.prod(img_shape)/(patch_size**2*img_channels)))
-        patch_size = img_size//num_patches
+        num_patches = int(np.prod(img_shape)/(patch_size**2*img_channels))
+        patch_size = img_size//int(np.sqrt(num_patches))
         token_dim = img_channels*patch_size**2
         
         self.to_patch = nn.Conv2d(img_channels, token_dim, kernel_size=patch_size, stride=patch_size)
         self.flatten_patches = nn.Flatten(2)
         self.linear_projection = nn.Linear(token_dim, token_dim)
         self.cls_token = nn.Parameter(torch.zeros(1, 1, token_dim))
-        self.pos_embedding = nn.Parameter(torch.zeros(1, num_patches**2 + 1, token_dim))
+        self.pos_embedding = nn.Parameter(torch.zeros(1, num_patches + 1, token_dim))
         self.transformer_encoder = TransformerEncoder(num_layers, token_dim, num_heads, mlp_size, dropout)
         self.mlp_head = MLP(token_dim, mlp_size, dropout)
         
     def forward(self, x, mask=None):
+        # input x: (batch_size, img_channels, img_size, img_size)
+        
         # make patches and flatten
+        # (batch_size, token_dim, sqrt(num_patches), sqrt(num_patches))
         x = self.to_patch(x)
+        # (batch_size, token_dim, sqrt(num_patches), sqrt(num_patches)) -> (batch_size, token_dim, num_patches) -> (batch_size, num_patches, token_dim)
         x = self.flatten_patches(x).transpose(1, 2)
         
         # linear projection of flattend patches
+        # (batch_size, num_patches, token_dim)
         x = self.linear_projection(x)
         
         # concat cls token
+        # (batch_size, num_patches + 1, token_dim)
         x = torch.cat((self.cls_token.expand(x.shape[0], -1, -1), x), dim=1)
         
         # patch + position embedding
+        # (batch_size, num_patches + 1, token_dim)
         x = x + self.pos_embedding
         
         # transformer encoder
+        # (batch_size, num_patches + 1, token_dim)
         x = self.transformer_encoder(x)
         
         # mlp head
+        # (batch_size, num_patches + 1, token_dim)
         x = self.mlp_head(x)
         
         return x
--- ViT/vistion_transformer/modules.py
+++ ViT/vistion_transformer/modules.py
@@ -20,21 +20,28 @@ class MSA(nn.Module):
         batch_size, num_patches, token_dim = x.shape
         head_dim = token_dim//self.n_heads
         
+        # num_patches: number of patches(cls_token included)
+        # (batch_size, num_patches, n_heads, head_dim) -> (batch_size, n_heads, num_patches, head_dim)
         query = self.fc_q(x).view(batch_size, num_patches, self.n_heads, head_dim).permute(0, 2, 1, 3)
         key = self.fc_k(x).view(batch_size, num_patches, self.n_heads, head_dim).permute(0, 2, 1, 3)
         value = self.fc_v(x).view(batch_size, num_patches, self.n_heads, head_dim).permute(0, 2, 1, 3)
         
+        # (batch_size, n_heads, num_patches, head_dim)@(batch_size, n_heads, head_dim, num_patches) -> (batch_size, n_heads, num_patches, num_patches)
         energy = query@key.transpose(-2, -1)/np.sqrt(head_dim)
         
         if mask is not None:
             energy = energy.masked_fill(mask == 0, -1e10)
-            
+        
+        # (batch_size, n_heads, num_patches, num_patches)
         attention = torch.softmax(energy, dim=-1)
         
+        # (batch_size, n_heads, num_patches, num_patches)@(batch_size, n_heads, num_patches, head_dim) -> (batch_size, n_heads, num_patches, head_dim)
         x = self.dropout(energy)@value
+        # (batch_size, num_patches, n_heads, head_dim)
         x = x.permute(0, 2, 1, 3).contiguous()
         x = x.view(batch_size, num_patches, token_dim)
         
+        # (batch_size, num_patches, token_dim)
         x = self.fc_o(x)
         
         return x, attention
