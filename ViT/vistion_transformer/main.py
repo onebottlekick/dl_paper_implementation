57e0de03ccc1c75ee39cb97086bbb76e952d798e
onebottlekick <gksqudcks97@gmail.com>
2022-07-22 14:11:02 +0900

    - Trainer - LR_scheduler

--- ViT/vistion_transformer/main.py
+++ ViT/vistion_transformer/main.py
@@ -3,21 +3,22 @@ import argparse
 import torch
 import torch.nn as nn
 
-from model import ViT
-from utils import build_model
 from configs import config_dict
-
+from model import ViT
+from utils import Trainer, WarmupCosineSchedule, WarmupLinearSchedule, build_model, get_transform, get_data_loader
 
 parser = argparse.ArgumentParser(description='Experiment')
-# parser.add_argument('data_path', metavar='DIR')
+parser.add_argument('data_path', metavar='DIR')
 parser.add_argument('-a', '--arch', metavar='ARCH', default='b16', help='model architecture (default: b16)')
 parser.add_argument('-e', '--epochs', default=100, type=int, help='number of epochs (default: 100)')
 parser.add_argument('-b', '--batch-size', default=64, type=int, metavar='N', help='mini-batch size (default: 64)')
 parser.add_argument('-o', '--optimizer', default='adam', type=str, metavar='OPT', help='optimizer (default: adam)')
-parser.add_argument('--lr', '--learning-rate', default=0.1, type=float, dest='lr')
+parser.add_argument('-lr', '--learning-rate', default=1e-3, type=float, dest='lr')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
 parser.add_argument('--weight-decay', default=1e-4, type=float, metavar='W', help='weight decay (default: 1e-4)', dest='weight_decay')
 parser.add_argument('-d', '--device', default='cuda', type=str, help='device to use (default: cuda)')
+parser.add_argument('-s', '--scheduler', default='warmup_cosine', type=str, help='scheduler (default: warmup_cosine)')
+parser.add_argument('-t', '--topk', default=(1, ), type=tuple, help='topk acc (default: (1, ))')
 
 best_acc1 = 0
 
@@ -26,54 +27,25 @@ optimizer_dict = {
     'sgd': torch.optim.SGD,
 }
 
-'''
-TODO 
-- lr decay
-- dataloader
-'''   
+scheduler_dict = {
+    'warmup_linear': WarmupLinearSchedule,
+    'warmup_cosine': WarmupCosineSchedule,
+}
+
 def main():
     args = parser.parse_args()
-    device = args.device
-    train_loader = None
-    val_loader = None
-    
     model_config = config_dict[args.arch]
+    
+    device = args.device
+    transform = get_transform(model_config['img_size'])
+    train_loader, val_loader = get_data_loader(args.data_path, transform, args.batch_size, shuffle=True)
+    
     model = build_model(ViT, model_config).to(device)
     optimizer = optimizer_dict[args.optimizer.lower()](model.parameters(), lr=args.lr)
     criterion = nn.CrossEntropyLoss()
-    
-    for epoch in range(args.epochs):
-        train(train_loader, model, criterion, optimizer, epoch, device)
-        validate(val_loader, model, criterion, device)
-    
-'''
-TODO 
-- train time function
-- print progress
-- resume training function
-'''   
-def train(train_loader, model, criterion, optimizer, epoch, device):
-    model.train()
-    for i, (imgs, targets) in enumerate(train_loader):
-        imgs = imgs.to(device)
-        targets = targets.to(device)
-        outputs = model(imgs)
-        loss = criterion(outputs, targets)
-        
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-
-# TODO calc top acc
-def validate(val_loader, model, criterion, device):
-    model.eval()
-    for i, (imgs, targets) in enumerate(val_loader):
-        imgs = imgs.to(device)
-        targets = targets.to(device)
-        
-        outputs = model(imgs)
-        loss = criterion(outputs, targets)
+    scheduler = scheduler_dict[args.scheduler](optimizer, args.epochs//5, args.epochs)
+    trainer = Trainer(model, {'train':train_loader, 'validation':val_loader}, criterion, optimizer, scheduler, args.epochs, args.topk, device)
+    trainer.train()
         
 
 if __name__ == '__main__':
--- ViT/vistion_transformer/utils.py
+++ ViT/vistion_transformer/utils.py
@@ -1,6 +1,9 @@
+import math
 import os
 
 import torch
+from torch.optim.lr_scheduler import LambdaLR
+from tqdm import tqdm
 
 
 def pair(x):
@@ -64,3 +67,78 @@ def get_data_loader(data_path, transform, batch_size, shuffle, split_ratio=0.7):
     val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=shuffle)
     
     return train_loader, val_loader
+
+
+class WarmupLinearSchedule(LambdaLR):
+    def __init__(self, optimizer, warmup_steps, t_total, last_epoch=-1):
+        self.warmup_steps = warmup_steps
+        self.t_total = t_total
+        super().__init__(optimizer, self.lr_lambda, last_epoch=last_epoch)
+        
+    def lr_lambda(self, step):
+        if step < self.warmup_steps:
+            return float(step) / float(max(1.0, self.warmup_steps))
+        return max(0.0, float(self.t_total - step) / float(max(1.0, self.t_total - self.warmup_steps)))
+    
+    
+class WarmupCosineSchedule(LambdaLR):
+    def __init__(self, optimizer, warmup_steps, t_total, cycles=.5, last_epoch=-1):
+        self.warmup_steps = warmup_steps
+        self.t_total = t_total
+        self.cycles = cycles
+        super(WarmupCosineSchedule, self).__init__(optimizer, self.lr_lambda, last_epoch=last_epoch)
+
+    def lr_lambda(self, step):
+        if step < self.warmup_steps:
+            return float(step) / float(max(1.0, self.warmup_steps))
+        progress = float(step - self.warmup_steps) / float(max(1, self.t_total - self.warmup_steps))
+        return max(0.0, 0.5 * (1. + math.cos(math.pi * float(self.cycles) * 2.0 * progress)))
+
+
+class Trainer:
+    def __init__(self, model, dataloader_dict, criterion, optimizer, scheduler, num_epochs, topk, device):
+        self.model = model.to(device)
+        self.best_acc = 0.0
+        self.dataloader_dict = dataloader_dict
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.num_epochs = num_epochs
+        self.topk = topk
+        self.device = device
+        self.running_loss, self.running_acc = {}, {}
+    
+    # TODO calc topk acc
+    def train(self):
+        for epoch in range(self.num_epochs):
+            for phase in ['train', 'validation']:
+                if phase == 'train':
+                    self.model.train()
+                else:
+                    self.model.eval()
+
+                self.running_loss[phase], self.running_acc[phase] = 0.0, 0.0
+
+                with tqdm(self.dataloader_dict[phase], unit='Batch') as t:
+                    t.set_description(f'{phase} Epoch: {epoch+1}')
+                    for inputs, targets in t:
+                        inputs = inputs.to(self.device)
+                        targets = targets.to(self.device)
+
+                        self.optimizer.zero_grad()
+
+                        with torch.set_grad_enabled(phase == 'train'):
+                            outputs, _ = self.model(inputs)
+                            loss = self.criterion(outputs, targets)
+
+                            t.set_postfix(loss=f'{loss.item():.6f}')
+
+                            if phase == 'train':
+                                loss.backward()
+                                self.optimizer.step()
+
+                    if phase == 'train':
+                        self.scheduler.step()
+                    else:
+                        print()
+                        print()
