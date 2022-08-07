752e5381cea4e9f17fb983942e6154121dab9a46
onebottlekick <gksqudcks97@gmail.com>
2022-08-07 14:45:41 +0900

    experiments shell

---
+++ Super_Resolution/SRCNN/experiments/experiments.ps1
@@ -0,0 +1,9 @@
+python experiments.py --lr_path ./test/demo/lr/butterfly.png `
+                      --lr_scale 3 `
+                      --model_path --model_path $(model_path) `
+                      --n_rows1 8 `
+                      --n_cols1 8 `
+                      --first_layer_fig_name patch_extraction.png `
+                      --n_rows1 4 `
+                      --n_cols1 8 `
+                      --second_layer_fig_name non_linear_mapping.png ;
